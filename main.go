daf5ab7d6c12d9393004423f783e2a6713b4f577
jhleeO <47955896+jhleeO@users.noreply.github.com>
2021-07-15 17:12:19 +0900

    slowURL Checker

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
+	results := make(map[string]string)
 	urls := []string{
 		"https://www.airbnb.com",
 		"https://www.naver.com",
@@ -21,15 +22,24 @@ func main() {
 	}
 
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
 func hitURL(url string) error {
-	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
 
 	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
 		return errRequestFailed
 	}
 	return nil
