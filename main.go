3125a24a62d527275659725f0e4cdd652627901a
jhleeO <47955896+jhleeO@users.noreply.github.com>
2021-07-15 22:14:35 +0900

    FAST URLChecker

--- main.go
+++ main.go
@@ -2,24 +2,48 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
 )
 
-func main() {
-	c := make(chan string)
-	people := [5]string{"Hippo", "koala", "Lion", "Tiger", "Python"}
+type result struct {
+	url string
+	status string
+}
 
-	for _, person := range people {
-		go isGood(person, c)
+func main() {
+	results := make(map[string]string)
+	c := make(chan result)
+
+	urls := []string {
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co",
+	}
+	for _, url := range urls {
+		go hitURL(url, c)
 	}
 
-	for i := 0; i < len(people); i++ {
-		fmt.Println(<-c)
+	for i := 0; i < len(urls); i++ {
+		r := <- c
+		results[r.url] = r.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
-func isGood(person string, c chan string) {
-	time.Sleep(time.Second * 5)
-	fmt.Println(person)
-	c <- person + " is good"
+func hitURL(url string, c chan<- result) {
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	c <- result{url, status}
 }
